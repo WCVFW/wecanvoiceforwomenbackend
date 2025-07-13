86cf61a112cd5551d3de973c63b3460c0f123abe
WCVFW <wcvfw2019@gmail.com>
2025-07-13 14:12:01 +0530

    Update ContactFormController.java

--- src/main/java/com/womensfoundation/controller/ContactFormController.java
+++ src/main/java/com/womensfoundation/controller/ContactFormController.java
@@ -12,7 +12,7 @@ import java.util.*;
 @RestController
 @RequestMapping("/api/contact")
 @CrossOrigin(origins = {
-    "https://wecanvoiceforwomen.org:8080",
+    "http://13.60.235.97/",
     "http://localhost:5173" // optional for local testing
 })
 public class ContactFormController {
