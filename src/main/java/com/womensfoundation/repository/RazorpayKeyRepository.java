511305f0e0746e800a213f2146e7f94af0b4509e
WCVFW <wcvfw2019@gmail.com>
2025-07-14 12:43:28 +0530

    Trigger deploy

--- src/main/java/com/womensfoundation/repository/RazorpayKeyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.womensfoundation.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.womensfoundation.model.RazorpayKey;
-
-public interface RazorpayKeyRepository extends JpaRepository<RazorpayKey, Long> {
-    RazorpayKey findTopByOrderByIdDesc(); // Gets the latest added key
-}
